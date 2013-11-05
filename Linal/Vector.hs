c120eeed90d65d8ea171e5740c9167d179350552
Anton Karamanov <ataraxer@gmail.com>
2013-11-05 11:57:49 +0400

    Add operators for vectors

--- Linal/Vector.hs
+++ Linal/Vector.hs
@@ -1,39 +1,52 @@
 module Linal.Vector
-( Vector(Vec3)
-, cross
-, dot
-, vdiff
-, normalize
-) where
+where
 
 import Control.Applicative
 
 data Vector a = Vec3 a a a
-	deriving (Show, Eq)
+    deriving (Show, Eq)
 
 instance Functor Vector where
-	fmap f (Vec3 x y z) = Vec3 (f x) (f y) (f z)
+    fmap f (Vec3 x y z) = Vec3 (f x) (f y) (f z)
 
 
-fromList :: [a] -> Vector a
-fromList [x, y, z] = Vec3 x y z
+fromList :: [Double] -> Vector Double
+fromList [x, y, z] = Vec3 (x :: Double) (y :: Double) (z :: Double)
 
 coords (Vec3 x y z) = [x, y, z]
 square = (**2)
 
-{-magnitude :: (Num a) => Vector a -> Double-}
 magnitude = sqrt . sum . coords . fmap square
 
-{-normalize :: (Num a) => Vector a -> Vector a-}
 normalize v = fmap (/m) v
-	where m = magnitude v
+    where m = magnitude v
 
-relate f a b = getZipList $ f <$> list a <*> list b
-	where list = ZipList . coords
+relate f a b = zipWith f (coords a) (coords b)
+{-relate f a b = getZipList $ f <$> list a <*> list b-}
+    {-where list = ZipList . coords-}
 
 dot a b = sum $ relate (*) a b
 
 cross (Vec3 ax ay az) (Vec3 bx by bz) =
-	Vec3 (ay*bz - az*by) (az*bx - ax*bz) (ax*by - ay*bx)
+    Vec3 (ay*bz - az*by) (az*bx - ax*bz) (ax*by - ay*bx)
 
-vdiff a b = fromList $ relate (-) a b
+vop f a b = fromList $ relate f a b
+
+a ->+ b = vop (+) a b
+a ->- b = vop (-) a b
+a ->* b = vop (*) a b
+a ->/ b = vop (/) a b
+
+v $>+ s = fmap (+s) v
+v $>- s = fmap (-s) v
+v $>* s = fmap (*s) v
+v $>/ s = fmap (/s) v
+v $>** s = fmap (**s) v
+
+a >.< b = dot a b
+a >#< b = cross a b
+
+vadd  = vop (+)
+vdiff = vop (-)
+vmult = vop (*)
+vdiv  = vop (/)
