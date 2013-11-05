83f0c00978de4e1179d321a7545dc749d5b4323a
Anton Karamanov <ataraxer@gmail.com>
2013-11-05 11:59:35 +0400

    Fix indentation levels (spaces over tabs)

--- Main.hs
+++ Main.hs
@@ -4,24 +4,33 @@ import Camera
 
 
 main = do
-	putStrLn "Bad-Ass Haskell Raytracer v0.1"
-	let image = render 640 480
-	putStrLn "Done!"
+    putStrLn "Bad-Ass Haskell Raytracer v0.1"
+    let image = render 640 480
+    putStrLn "Done!"
 
 render w h = do
-	x <- [0..w-1]
-	y <- [0..h-1]
-	return $ renderPixel (x, y) (w, h)
+    x <- [0..w-1]
+    y <- [0..h-1]
+    return $ renderPixel (x, y) (w, h)
 
 renderPixel c s = traceRay $ rayToPixel sceneCamera c s
-	where sceneCamera = camera (Vec3 0 0 0) (Vec3 0 0 0)
+    where sceneCamera = camera cameraPosition cameraCenter
+          cameraPosition = Vec3 3.0 1.5 (-4)
+          cameraCenter   = Vec3 0.5 0.0 0.0
 
 rayToPixel sceneCamera (x, y) (w, h) = Ray cameraPosition direction
-	where
-		epsilon = 0.5
-		xAmount = (x + epsilon) / w
-		yAmount = ((h - y) + epsilon) / h
-		direction = Vec3 0 0 0
-		cameraPosition = position sceneCamera
+    where epsilon        = 0.5
+          xAmount        = (x + epsilon) / w
+          yAmount        = ((h - y) + epsilon) / h
+          direction      = shiftCamera sceneCamera xAmount yAmount
+          cameraPosition = position sceneCamera
 
-traceRay ray = 0
+type Color = (Double, Double, Double)
+
+traceRay ray
+    | intersection /= Nothing = intersectionColor ray intersection
+    | otherwise               = clearColor
+    where clearColor = (0, 0, 0)
+          intersection = Nothing
+
+intersectionColor ray intersection = (0, 0, 0)
